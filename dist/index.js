3300f56ec3202b67f2804a1a9a36e8ebf012e400
Your Name <you@example.com>
2025-08-16 15:48:23 +0200

    update code

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("hello");
+const employee1 = {
+    fullName: ["John", "Doe"],
+    age: 30,
+    department: "Engineering",
+    skills: ["JavaScript", "Python", "c++"],
+    isActive: true,
+};
+const employee2 = {
+    fullName: ["Jane", "Smith"],
+    age: 25,
+    department: "Marketing",
+    skills: ["SEO", "Social Media", "Content Creation"],
+    isActive: false,
+};
+function logEmployee(employee) {
+    console.log("\n--- Employee Details---");
+    console.log('Name: ${employee.fullName[0]} ${employee.fullName[1]}');
+    console.log('Age: ${employee.age}');
+    console.log('Department: ${employee.department}');
+    console.log('Skills: ${employee.skills.join(",")}');
+    console.log('Active: ${employee.isActive ? "Yes" : "No"}');
+    console.log("------------------\n");
+}
+;
+logEmployee(employee1);
+logEmployee(employee2);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("hello")
+type Employee = {
+    fullName: [string, string];
+    age: number;
+    department: string;
+    skills: string[];
+    isActive: boolean;
+};
+
+const employee1: Employee = {
+    fullName: ["John", "Doe"],
+    age: 30,
+    department: "Engineering",
+    skills: ["JavaScript", "Python", "c++"],
+    isActive: true,
+};
+
+const employee2: Employee = {
+    fullName: ["Jane", "Smith"],
+    age: 25,
+    department: "Marketing",
+    skills: ["SEO", "Social Media", "Content Creation"],
+    isActive: false,
+};
+
+function logEmployee(employee: Employee): void {
+    console.log("\n--- Employee Details---");
+    console.log('Name: ${employee.fullName[0]} ${employee.fullName[1]}');
+    console.log('Age: ${employee.age}');
+    console.log('Department: ${employee.department}');
+    console.log('Skills: ${employee.skills.join(",")}');
+    console.log('Active: ${employee.isActive ? "Yes" : "No"}');
+    console.log("------------------\n");
+
+};
+
+logEmployee(employee1);
+logEmployee(employee2);
